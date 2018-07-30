f86075cee62ec3e4b4cdc71b9cd5136d5bad056e
Gede Lumbung <gedesumawijaya@gmail.com>
2018-07-30 23:05:03 +0800

    :recycle: remove edit button

--- src/component/Dashboard/Item.js
+++ src/component/Dashboard/Item.js
@@ -8,11 +8,6 @@ const Item = ({ product, onDelete }) => {
       <td>{product.title}</td>
       <td>{product.brand}</td>
       <td>
-        <Link to={`/dashboard/products/edit/${product.id}`}>
-          <span className="button is-dark is-small">
-            <i className="material-icons">edit</i> Edit
-          </span>
-        </Link>{" "}
         <span className="button is-danger is-small" onClick={onDelete(product.id)}>
           <i className="material-icons">delete</i> Delete
         </span>
